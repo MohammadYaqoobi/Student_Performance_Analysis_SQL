72cb26dd8088dd7a0b996668536608f9dd4fa7d6
Mohammad Yaqoobi <mohammad.yaqoobi143@gmail.com>
2025-12-23 12:07:22 +0430

    Update Query 2 Effectiveness of Educational Support Systems.sql

--- Query 2 Effectiveness of Educational Support Systems.sql
+++ Query 2 Effectiveness of Educational Support Systems.sql
@@ -6,8 +6,8 @@ SELECT
     COUNT(*) AS Student_Count,
     CAST(AVG(G1) AS DECIMAL(10,2)) AS Avg_First_Period_Grade,
     CAST(AVG(G3) AS DECIMAL(10,2)) AS Avg_Final_Grade,
-    -- محاسبه پیشرفت نمره از ترم اول تا آخر
     CAST(AVG(G3) - AVG(G1) AS DECIMAL(10,2)) AS Grade_Improvement
 FROM student_mat
 GROUP BY schoolsup, famsup, paid
+
 ORDER BY Avg_Final_Grade DESC;
