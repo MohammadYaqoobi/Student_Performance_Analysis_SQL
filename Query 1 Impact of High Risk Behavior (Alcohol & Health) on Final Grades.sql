3d772b4ccaf4568f777f9d10b756988add45f000
Mohammad Yaqoobi <mohammad.yaqoobi143@gmail.com>
2025-12-23 12:06:47 +0430

    Update Query 1 Impact of High Risk Behavior (Alcohol & Health) on Final Grades.sql

--- Query 1 Impact of High Risk Behavior (Alcohol & Health) on Final Grades.sql
+++ Query 1 Impact of High Risk Behavior (Alcohol & Health) on Final Grades.sql
@@ -8,11 +8,12 @@ SELECT
     AVG(health) AS Avg_Health_Score,
     AVG(absences) AS Avg_Absences,
     CAST(AVG(G3) AS DECIMAL(10,2)) AS Average_Final_Grade
-FROM student_mat -- نام جدول خود را چک کنید
+FROM student_mat
 GROUP BY 
     CASE 
         WHEN (Dalc + Walc) >= 6 THEN 'High Alcohol Consumption'
         WHEN (Dalc + Walc) BETWEEN 3 AND 5 THEN 'Moderate Consumption'
         ELSE 'Low Consumption'
     END
+
 ORDER BY Average_Final_Grade DESC;
