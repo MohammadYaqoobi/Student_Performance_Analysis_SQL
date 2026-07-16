2201642f569b8c63c0c48f32d3d8f3f9c3e7c3f4
Mohammad Yaqoobi <mohammad.yaqoobi143@gmail.com>
2026-07-16 22:35:34 +0430

    Update 03_school_comparison.sql

--- sql/03_school_comparison.sql
+++ sql/03_school_comparison.sql
@@ -1,25 +1,24 @@
 /*
 =============================================================================
-  SCHOOL PERFORMANCE COMPARISON
-  Management Question: Which school performs better and why?
-  Note: Controlling for parental education ensures fair comparison
-  Author: Mohammad Yaqoobi
+SCHOOL PERFORMANCE COMPARISON
+Management Question: Which school performs better, controlling for background?
+Note: Medu >= 3 means "secondary OR higher" (3 = secondary, 4 = higher).
+Author: Mohammad Yaqoobi
 =============================================================================
 */
-
-SELECT 
+SELECT
     school,
-    CASE 
-        WHEN Medu >= 3 THEN 'Higher Educated Mother'
-        ELSE 'Non-Higher Educated Mother'
+    CASE
+        WHEN Medu >= 3 THEN 'Secondary+ Educated Mother'
+        ELSE 'Lower Educated Mother'
     END AS mother_education,
     COUNT(*) AS student_count,
     CAST(AVG(G3) AS DECIMAL(10,2)) AS avg_final_grade
 FROM student_mat
-GROUP BY school, 
-    CASE 
-        WHEN Medu >= 3 THEN 'Higher Educated Mother'
-        ELSE 'Non-Higher Educated Mother'
+GROUP BY school,
+    CASE
+        WHEN Medu >= 3 THEN 'Secondary+ Educated Mother'
+        ELSE 'Lower Educated Mother'
     END
 HAVING COUNT(*) >= 10
 ORDER BY school, mother_education;
